14f0ed880ad735e10a6959ee7378c00e50d2ac1a
Saul Moreira <saul.ms@hotmail.com>
2019-11-14 00:38:04 -0300

    Projeto Criado

---
+++ src/test/java/com/demo/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
